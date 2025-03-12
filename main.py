13bef514594b92cdf24d5433195b49aa938eb81c
Alena Kozlova <alena.kozlova2004@mail.ru>
2025-03-12 11:07:06 +0300

    add parser for urls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import socket
 
 from utils.loader.toml import TomlLoader
+from utils.parser.url import UrlParser
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -12,27 +13,27 @@ if __name__ == '__main__':
     password = config['user']['password']
     server_url = config['servers']['sms']['url']
 
-    # host, port, uri = parse_url(server_url)
-    #
-    # client.connect((host, port))
-    #
-    # request = (b'POST /send_sms HTTP/1.1\r\n'
-    #            b'Host: localhost:4010\r\n'
-    #            b'Authorization:Basic dXNlcjoxMjM0NQ==Content-Type: application/json\r\n'
-    #            b'Content-Length: 72\r\n'
-    #            b'\r\n{"sender": "88005553535", "recipient": "88005553536", "message": "test"}')
-    #
-    # client.sendall(request)
-    #
-    # response = b""
-    # while True:
-    #     chunk = client.recv(4096)
-    #
-    #     if len(chunk) == 0:
-    #         break
-    #
-    #     response = response + chunk
-    #
-    # print(response)
-    #
-    # client.close()
+    host, port, uri = UrlParser.parse(server_url)
+
+    client.connect((host, port))
+
+    request = (b'POST /send_sms HTTP/1.1\r\n'
+               b'Host: localhost:4010\r\n'
+               b'Authorization:Basic dXNlcjoxMjM0NQ==Content-Type: application/json\r\n'
+               b'Content-Length: 72\r\n'
+               b'\r\n{"sender": "88005553535", "recipient": "88005553536", "message": "test"}')
+
+    client.sendall(request)
+
+    response = b""
+    while True:
+        chunk = client.recv(4096)
+
+        if len(chunk) == 0:
+            break
+
+        response = response + chunk
+
+    print(response)
+
+    client.close()
---
+++ utils/parser/url.py
@@ -0,0 +1,26 @@
+class UrlParser:
+    """
+    A class for parsing strings with URL.
+    """
+    @staticmethod
+    def parse(url: str) -> tuple[str, int, str]:
+        """
+        Selects the URL string.
+
+        :param url: The URL string.
+        :return: The tuple from host, port, URI of URL.
+        """
+        try:
+            if url.startswith("http://"):
+                url = url[7:]
+
+            url_parts = url.split("/", 1)
+            host_port, uri = url_parts[0], "/" + url_parts[1]
+            host, port = host_port.split(":")
+            port = int(port)
+
+            return host, port, uri
+        except ValueError as e:
+            raise ValueError(f"Value error in URL: {e}")
+        except Exception as e:
+            raise Exception(f"Unexpected error occurred: {e}")
