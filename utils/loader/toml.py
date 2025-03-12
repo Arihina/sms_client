9c5b0d47947aca8602ec47fd7432b6804f4bbcc8
Alena Kozlova <alena.kozlova2004@mail.ru>
2025-03-12 10:55:00 +0300

    add try except

--- utils/loader/toml.py
+++ utils/loader/toml.py
@@ -16,8 +16,16 @@ class TomlLoader(BaseLoader):
         :param path: The path to the configuration file to load.
         :return: A dictionary with configuration data. Keys are strings, and values can be any.
         """
+        try:
+            with open(path, "rb") as f:
+                data = tomllib.load(f)
 
-        with open(path, "rb") as f:
-            data = tomllib.load(f)
-
-        return data
+            return data
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found at path: {path}")
+        except tomllib.TOMLDecodeError as e:
+            raise tomllib.TOMLDecodeError(f"Error decoding TOML file: {e}")
+        except ValueError as e:
+            raise ValueError(f"Value error in TOML file: {e}")
+        except Exception as e:
+            raise Exception(f"Unexpected error occurred: {e}")
